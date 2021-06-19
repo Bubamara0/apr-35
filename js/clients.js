d71cb3468224aac6614e101f90e57eb181a8fe8a
Bubamara0 <72171393+Bubamara0@users.noreply.github.com>
2021-06-19 15:24:01 +0200

    Delete clients.js

--- js/clients.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function(){
-    const apparitionRemerciements = document.querySelector("#texte_presentation");
-    let scroll = window.scrollY;
-    let hauteur = 200;
-    if (scroll >= 200) {
-        apparitionRemerciements.style.opacity = "100%";
-      }
-    }
-  );
